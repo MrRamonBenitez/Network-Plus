a03146b1b502699ac4b3c47aa329041868ae9cb9
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-01 16:51:41 +0500

    Added additional logic to the MainServer class

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,27 +1,33 @@
 import java.io.*;
 import java.net.*;
 
-public class MainServer {
+public class MainServer implements AutoCloseable {
+
     public static void main(String[] args) throws IOException {
         System.out.println("Server started");
         int port = 8083;
 
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ){
 
             out.println("Greetings, good friend! Say your name->");
 
             String msg = in.readLine();
-            out.println(String.format("Hi, %s, do you want to buy football tickets? (yes/no)", msg));
-
+                if (null == msg) {
+                    break;
+                } else {
+                    out.println(String.format("Hi, %s, do you want to buy football tickets? (yes/no)", msg));
+                }
             msg = in.readLine();
             if ("yes".equals(msg)) {
                 out.println("One football ticket costs 10. How many tickets would you like to purchase?");
             } else {
                 out.println("All the best! See you next time!");
+                break;
             }
 
             msg = in.readLine();
@@ -29,6 +35,7 @@ public class MainServer {
                 out.println(String.format("Ticket price will be %d euros. Buy it? (yes/no)", Integer.parseInt(msg) * 10));
             } else {
                 out.println("All the best! See you next time!");
+                break;
             }
 
             msg = in.readLine();
@@ -36,11 +43,17 @@ public class MainServer {
                 out.println("Thank you for your purchase! See you next time!");
             } else {
                 out.println("All the best! See you next time!");
+                break;
             }
-
-
-            serverSocket.close();
         }
     }
+
+}
+
+    @Override
+    public void close() {
+        System.out.println("Close");
+    }
+
 }
 
